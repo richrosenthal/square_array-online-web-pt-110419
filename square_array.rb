06d21b10a3981927aa25cd536f64ce4d3cee9f07
Richard Rosenthal <richard.rosenthal@acd.ccac.edu>
2019-11-11 19:52:04 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,9 @@
 def square_array(array)
+  new_array = [] 
   array.each do |integer|
-    array[integer * integer] 
-  end
+   # array[integer *  integer] 
+   new_array << integer ** 2
+   
 end
-  array
+  new_array
 end
